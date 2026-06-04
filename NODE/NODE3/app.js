c41082f2199dabf417f51ab158360f265363da60
ritickkumar <ritickkumar08@gmail.com>
2026-06-04 13:27:13 +0530

    revision is done

---
+++ NODE/NODE3/app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) =>{
+    res.send("hello mister")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`listening to port ${PORT}`);
+})
